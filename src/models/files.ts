f8e775af5cbc468720a65b50e0f0eff546964b73
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2022-05-05 01:08:18 +0100

    fixed class FilesModel to be able to instanciate it

--- src/models/files.ts
+++ src/models/files.ts
@@ -1,6 +1,14 @@
-export interface FilesModel {
+export class FilesModel {
     uuid: string;
     name: string;
     description: string;
     creationDate: Date;
+
+
+  constructor() {
+    this.uuid = "";
+    this.name = "";
+    this.description = "";
+    this.creationDate = new Date();
+  }
 }
