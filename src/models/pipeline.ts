1048384215cd5e1cb53f8b0d5f1f2bda685d5f91
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2022-06-20 02:10:26 +0100

    added missing fields on class CreatedPipeline

--- src/models/pipeline.ts
+++ src/models/pipeline.ts
@@ -2,6 +2,7 @@
  * A pipeline that was just created and, therefore, doesn't have output datasets yet.
  */
 import {Command} from "./Command";
+import {FilesModel} from "./files";
 
 export class CreatedPipeline {
   uuid: string;
@@ -26,9 +27,14 @@ export class FinishedPipeline extends CreatedPipeline {
   outputDatasetsUuids: string[];
   command: Command;
 
+  inputDatasets: FilesModel[];
+  outputDatasets: FilesModel[];
+
   constructor() {
     super();
     this.outputDatasetsUuids = new Array();
     this.command = new Command();
+    this.inputDatasets = [];
+    this.outputDatasets = [];
   }
 }
